555b5f4d97893b0eeba988258625e32725d44902
ragnarok04 <tanmay8265@gmail.com>
2024-05-02 17:07:44 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,116 +7,9 @@ import WaveSurfer from 'wavesurfer.js';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements AfterViewInit,OnInit  {
-  @ViewChild('inputField') inputField!: ElementRef;
-  title = 'assignment';
-  contests = false;
-  matches = false;
-  displayedColumns: string[] = ['option', 'radar', 'falcon', 'sinlimit','sinrisk', 'sinbet', 'stake', 'pnl', 'multirisk', 'multidist'];
-  dataSource = new MatTableDataSource<any>();
-  // sports: Sport[] = [
-  //   {
-  //     name: 'Cricket',
-  //     contests: [
-  //       { name: 'IPL', matches: ['Match 1', 'Match 2'] }
-  //     ]
-  //   },
-  //   {
-  //     name: 'Football',
-  //     contests: [
-  //       { name: 'FIFA', matches: ['Match 1', 'Match 2'] }
-  //     ]
-  //   },  {
-  //     name: 'Cricket',
-  //     contests: [
-  //       { name: 'IPL', matches: ['Match 1', 'Match 2'] }
-  //     ]
-  //   },
-  //   {
-  //     name: 'Football',
-  //     contests: [
-  //       { name: 'FIFA', matches: ['Match 1', 'Match 2'] }
-  //     ]
-  //   },  {
-  //     name: 'Cricket',
-  //     contests: [
-  //       { name: 'IPL', matches: ['Match 1', 'Match 2'] }
-  //     ]
-  //   },
-  //   {
-  //     name: 'Football',
-  //     contests: [
-  //       { name: 'FIFA', matches: ['Match 1', 'Match 2'] }
-  //     ]
-  //   },  {
-  //     name: 'Cricket',
-  //     contests: [
-  //       { name: 'IPL', matches: ['Match 1', 'Match 2'] }
-  //     ]
-  //   },
-  //   {
-  //     name: 'Football',
-  //     contests: [
-  //       { name: 'FIFA', matches: ['Match 1', 'Match 2'] }
-  //     ]
-  //   },  {
-  //     name: 'Cricket',
-  //     contests: [
-  //       { name: 'IPL', matches: ['Match 1', 'Match 2'] }
-  //     ]
-  //   },
-  //   {
-  //     name: 'Football',
-  //     contests: [
-  //       { name: 'FIFA', matches: ['Match 1', 'Match 2'] }
-  //     ]
-  //   },
-  // ];
-  data:any;
-  selectedRow: any;
 
-  
-  // constructor() {
-  // }
   ngOnInit(): void {
-    this.data=ELEMENT_DATA;
-    this.dataSource = new MatTableDataSource(ELEMENT_DATA);
-console.log(this.dataSource);
-// this.loadVideo();
   }
-  // ngAfterViewInit() {
-  //   this.removeExpansionPanelBodyPadding();
-  // }
-
-  removeExpansionPanelBodyPadding() {
-    const expansionPanelBodies = document.getElementsByClassName('mat-expansion-panel-body');
-    for (let i = 0; i < expansionPanelBodies.length; i++) {
-      const expansionPanelBody = expansionPanelBodies[i] as HTMLElement;
-      expansionPanelBody.style.padding = '0';
-    }
-  }
-  toggleEditMode(element: any) {
-    this.dataSource.data.forEach(item => item.editMode = false); // Close edit mode for all elements
-    element.editMode = true; // Open edit mode for the clicked element
-    setTimeout(() => this.inputField.nativeElement.focus()); // Set focus on the input after a short delay
-  }
-  
-  navigate(direction: string, element: any) {
-    const index = this.dataSource.data.indexOf(element);
-  
-    if (direction === 'up' && index > 0) {
-      const previousElement = this.dataSource.data[index - 1];
-      this.toggleEditMode(previousElement); // Toggle edit mode for previous element
-    } else if (direction === 'down' && index < this.dataSource.data.length - 1) {
-      const nextElement = this.dataSource.data[index + 1];
-      this.toggleEditMode(nextElement); // Toggle edit mode for next element
-    }
-  }
- 
-  saveAndToggleEditMode(element: any) {
-    element.editMode = false;
-    setTimeout(() => this.inputField.nativeElement.focus());
-  }
-  
   @ViewChild('videoPlayer') videoPlayer!: ElementRef;
   @ViewChild('waveformCanvas') waveformCanvas!: ElementRef;
   isPlaying = true;
@@ -134,7 +27,6 @@ console.log(this.dataSource);
 
   ngAfterViewInit(): void {
     const videoElement = this.videoPlayer.nativeElement;
-
     const source = this.audioCtx.createMediaElementSource(videoElement);
     source.connect(this.analyser);
     this.analyser.connect(this.audioCtx.destination);
@@ -142,6 +34,14 @@ console.log(this.dataSource);
     this.drawWaveform();
   }
   togglePlay() {
+    if (this.audioCtx.state === 'suspended') {
+      document.addEventListener('click', () => {
+        this.audioCtx.resume().then(() => {
+        }).catch((error) => {
+          console.error('Failed to resume AudioContext:', error);
+        });
+      }, { once: true }); // Ensure the event listener only triggers once
+    }
     if (this.videoPlayer.nativeElement.paused) {
       this.videoPlayer.nativeElement.play();
     } else {
@@ -155,6 +55,8 @@ console.log(this.dataSource);
       const frameRate = 30; // Assuming a frame rate of 30 frames per second
       this.videoPlayer.nativeElement.currentTime = currentTime + (1 / frameRate);
     }
+    this.drawWaveform();
+
   }
 
   moveOneFrameBackward() {
@@ -163,6 +65,8 @@ console.log(this.dataSource);
       const frameRate = 30; // Assuming a frame rate of 30 frames per second
       this.videoPlayer.nativeElement.currentTime = currentTime - (1 / frameRate);
     }
+    this.drawWaveform();
+
   }
 
   moveOneSecondForward() {
@@ -170,6 +74,7 @@ console.log(this.dataSource);
       const currentTime = this.videoPlayer.nativeElement.currentTime;
       this.videoPlayer.nativeElement.currentTime = currentTime + 1;
     }
+    this.drawWaveform();
   }
 
   moveOneSecondBackward() {
@@ -177,6 +82,8 @@ console.log(this.dataSource);
       const currentTime = this.videoPlayer.nativeElement.currentTime;
       this.videoPlayer.nativeElement.currentTime = currentTime - 1;
     }
+    this.drawWaveform();
+
   }
   drawWaveform(): void {
     const canvas = this.waveformCanvas.nativeElement;
@@ -221,29 +128,3 @@ console.log(this.dataSource);
     cancelAnimationFrame(this.requestAnimationFrameId);
   }
 }
-export interface Sport {
-  name: string;
-  contests: Contest[];
-}
-
-export interface Contest {
-  name: string;
-  matches: string[];
-}
-
-export interface PeriodicElement {
-  option: string;
-  radar: string;
-  falcon: string;
-  sinlimit: string;
-  sinrisk: string;
-  sinbet: string;
-  stake: string;
-  pnl: string;
-  multirisk: string;
-  multidist: string;
-}
-const ELEMENT_DATA: PeriodicElement[] = [
-  {option: "1", radar: 'Hydrogen', falcon: "1.0079", sinlimit: 'H',sinrisk: 'H',sinbet: 'H',stake: 'H',pnl: 'H',multirisk: 'H',multidist: 'H',},
-  {option: "2", radar: 'Helium', falcon: "4.0026", sinlimit: 'He',sinrisk: 'H',sinbet: 'H',stake: 'H',pnl: 'H',multirisk: 'H',multidist: 'H',},
-];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatExpansionModule} from '@angular/material/expansion';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatTableModule} from '@angular/material/table';
 import { FormsModule } from '@angular/forms';
-import { UppercasePipe } from './uppercase.pipe';
 import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [
     AppComponent,
-    UppercasePipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatExpansionModule,
-    BrowserAnimationsModule,
-    MatTableModule,
     FormsModule,
     MatIconModule
   ],
